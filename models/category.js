17988aeaead4c8438f0e62829a60ab8e2836b216
rinuya <rinuya@users.noreply.github.com>
2022-06-12 00:48:52 +0200

    continue on populatedb.js tomorrow

---
+++ models/category.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema(
+    {
+        name: {type: String, required: true},
+        description: {type: String},
+    }
+  );
+
+// Virtual for book's URL
+CategorySchema
+.virtual('url')
+.get(function () {
+  return '/categories/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Category', CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema(
+    {
+      name: {type: String, required: true},
+      description: {type: String},
+      numberinstock: {type: Number, required: true},
+      price: {type: Number , required: true},
+      category: [{type: Schema.Types.ObjectId, ref: 'Category', required: true}]
+    }
+  );
+
+// Virtual for book's URL
+ItemSchema
+.virtual('url')
+.get(function () {
+  return '/items/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Item', ItemSchema);
