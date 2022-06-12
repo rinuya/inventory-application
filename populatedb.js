306c68acfd7482ed5fbfb620890c308c2d700b7d
rinuya <rinuya@users.noreply.github.com>
2022-06-12 13:09:59 +0200

    populated database with some data

--- populatedb.js
+++ populatedb.js
@@ -27,198 +27,147 @@ var categories = []
 
 //CONTINUE HERE ON SUNDAY
 
-function authorCreate(first_name, family_name, d_birth, d_death, cb) {
-  authordetail = {first_name:first_name , family_name: family_name }
-  if (d_birth != false) authordetail.date_of_birth = d_birth
-  if (d_death != false) authordetail.date_of_death = d_death
-  
-  var author = new Author(authordetail);
+function itemCreate(name, description, numberinstock, price, category, cb) {
+
+  var item = new Item(  
+    item = { 
+    name: name,
+    description: description,
+    numberinstock: numberinstock,
+    price: price,
+    category: category,
+  });
        
-  author.save(function (err) {
+  item.save(function (err) {
     if (err) {
       cb(err, null)
       return
     }
-    console.log('New Author: ' + author);
-    authors.push(author)
-    cb(null, author)
+    console.log('New Item: ' + item);
+    items.push(item)
+    cb(null, item)
   }  );
 }
 
-function genreCreate(name, cb) {
-  var genre = new Genre({ name: name });
+function categoryCreate(name, description, cb) {
+  var category = new Category({ 
+    name: name,
+    description: description,
+  });
        
-  genre.save(function (err) {
+  category.save(function (err) {
     if (err) {
       cb(err, null);
       return;
     }
-    console.log('New Genre: ' + genre);
-    genres.push(genre)
-    cb(null, genre);
+    console.log('New Category: ' + category);
+    categories.push(category)
+    cb(null, category);
   }   );
 }
 
-function bookCreate(title, summary, isbn, author, genre, cb) {
-  bookdetail = { 
-    title: title,
-    summary: summary,
-    author: author,
-    isbn: isbn
-  }
-  if (genre != false) bookdetail.genre = genre
-    
-  var book = new Book(bookdetail);    
-  book.save(function (err) {
-    if (err) {
-      cb(err, null)
-      return
-    }
-    console.log('New Book: ' + book);
-    books.push(book)
-    cb(null, book)
-  }  );
+function createCategories(cb) {
+  async.series([
+      function(callback) {
+        categoryCreate('Shirts', 'Regular shirts for everyday wear', callback);
+      },
+      function(callback) {
+        categoryCreate('Hoddies', 'Comfortable hoddies for chilly evening nights', callback);
+      },
+      function(callback) {
+        categoryCreate('Pullovers', 'Festive Pullovers!', callback);
+      },
+      function(callback) {
+        categoryCreate('Coats', 'Stylish coats', callback);
+      },
+      function(callback) {
+        categoryCreate('Pants', 'Pants!', callback);
+      },
+
+      ],
+      // optional callback
+      cb);
 }
+//category 0 would be tshirts
+//category 1 would be hoodies
+//category 2 would be pullovers
+//category 3 would be coats
+//category 4 would be pants
+function createItems(cb) {
+    async.parallel([
+        function(callback) {
+          itemCreate('Rick and Morty Shirt Unisex', 'A very cool shirt!', 34, 19.95, [categories[0],], callback);
+        },
+        function(callback) {
+          itemCreate('JuJuKai Shirt Unisex', 'The best shirt in this store!', 12, 39.95, [categories[0],], callback);
+        },
+        function(callback) {
+          itemCreate('BreakingBad Shirt Unisex', 'A very cool shirt!', 12, 29.95, [categories[0],], callback);
+        },
+        function(callback) {
+          itemCreate('White Shirt Unisex', 'A simple shirt', 32, 9.95, [categories[0],], callback);
+        },
+        function(callback) {
+          itemCreate('Black Shirt Unisex', 'A simple shirt', 17, 9.95, [categories[0],], callback);
+        },
 
+        function(callback) {
+          itemCreate('StreetArt Hoodie Unisex', 'A very cool hoddie!', 14, 39.95, [categories[1],], callback);
+        },
+        function(callback) {
+          itemCreate('Itadori Hoodie Unisex', 'A very cool hoddie!', 12, 49.95, [categories[1],], callback);
+        },
+        function(callback) {
+          itemCreate('BreakingBad Hoodie Unisex', 'A very cool hoddie!', 12, 39.95, [categories[1],], callback);
+        },
+        function(callback) {
+          itemCreate('White Hoodie Unisex', 'A simple hoddie', 32, 19.95, [categories[1],], callback);
+        },
+        function(callback) {
+          itemCreate('Black Hoodie Unisex', 'A simple hoddie', 17, 19.95, [categories[1],], callback);
+        },
 
-function bookInstanceCreate(book, imprint, due_back, status, cb) {
-  bookinstancedetail = { 
-    book: book,
-    imprint: imprint
-  }    
-  if (due_back != false) bookinstancedetail.due_back = due_back
-  if (status != false) bookinstancedetail.status = status
-    
-  var bookinstance = new BookInstance(bookinstancedetail);    
-  bookinstance.save(function (err) {
-    if (err) {
-      console.log('ERROR CREATING BookInstance: ' + bookinstance);
-      cb(err, null)
-      return
-    }
-    console.log('New BookInstance: ' + bookinstance);
-    bookinstances.push(bookinstance)
-    cb(null, book)
-  }  );
-}
+        function(callback) {
+          itemCreate('Thanksgiving Pullover Unisex', 'A turkey themed pullover!', 7, 99.95, [categories[2],], callback);
+        },
+        function(callback) {
+          itemCreate('Winter Pullover Unisex', 'A pullover!', 11, 69.95, [categories[2],], callback);
+        },
+        function(callback) {
+          itemCreate('Holiday Pullover Unisex', 'A very cool pullover!', 9, 49.95, [categories[2],], callback);
+        },
+        function(callback) {
+          itemCreate('Christmas Pullover Unisex', 'A very cool pullover!', 3, 59.95, [categories[2],], callback);
+        },
 
+        function(callback) {
+          itemCreate('Black Coat Unisex', 'Stylish coat!', 3, 159.95, [categories[3],], callback);
+        },
 
-function createGenreAuthors(cb) {
-    async.series([
         function(callback) {
-          authorCreate('Patrick', 'Rothfuss', '1973-06-06', false, callback);
+          itemCreate('Black Techwear Pants Unisex', 'Modern pants!', 32, 59.95, [categories[4],], callback);
         },
         function(callback) {
-          authorCreate('Ben', 'Bova', '1932-11-8', false, callback);
+          itemCreate('Striped Techwear Pants Unisex', 'Modern pants!', 33, 59.95, [categories[4],], callback);
         },
         function(callback) {
-          authorCreate('Isaac', 'Asimov', '1920-01-02', '1992-04-06', callback);
+          itemCreate('Blue Jeans Unisex', 'Old fashioned Blue jeans!', 63, 59.95, [categories[4],], callback);
         },
-        function(callback) {
-          authorCreate('Bob', 'Billings', false, false, callback);
-        },
-        function(callback) {
-          authorCreate('Jim', 'Jones', '1971-12-16', false, callback);
-        },
-        function(callback) {
-          genreCreate("Fantasy", callback);
-        },
-        function(callback) {
-          genreCreate("Science Fiction", callback);
-        },
-        function(callback) {
-          genreCreate("French Poetry", callback);
-        },
+
         ],
         // optional callback
         cb);
 }
-
-
-function createBooks(cb) {
-    async.parallel([
-        function(callback) {
-          bookCreate('The Name of the Wind (The Kingkiller Chronicle, #1)', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.', '9781473211896', authors[0], [genres[0],], callback);
-        },
-        function(callback) {
-          bookCreate("The Wise Man's Fear (The Kingkiller Chronicle, #2)", 'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.', '9788401352836', authors[0], [genres[0],], callback);
-        },
-        function(callback) {
-          bookCreate("The Slow Regard of Silent Things (Kingkiller Chronicle)", 'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.', '9780756411336', authors[0], [genres[0],], callback);
-        },
-        function(callback) {
-          bookCreate("Apes and Angels", "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...", '9780765379528', authors[1], [genres[1],], callback);
-        },
-        function(callback) {
-          bookCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...", '9780765379504', authors[1], [genres[1],], callback);
-        },
-        function(callback) {
-          bookCreate('Test Book 1', 'Summary of test book 1', 'ISBN111111', authors[4], [genres[0],genres[1]], callback);
-        },
-        function(callback) {
-          bookCreate('Test Book 2', 'Summary of test book 2', 'ISBN222222', authors[4], false, callback)
-        }
-        ],
-        // optional callback
-        cb);
-}
-
-
-function createBookInstances(cb) {
-    async.parallel([
-        function(callback) {
-          bookInstanceCreate(books[0], 'London Gollancz, 2014.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[1], ' Gollancz, 2011.', false, 'Loaned', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[2], ' Gollancz, 2015.', false, false, callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Maintenance', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Loaned', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[0], 'Imprint XXX2', false, false, callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[1], 'Imprint XXX3', false, false, callback)
-        }
-        ],
-        // Optional callback
-        cb);
-}
-
 
 
 async.series([
-    createGenreAuthors,
-    createBooks,
-    createBookInstances
+  createCategories,
+  createItems,
 ],
 // Optional callback
 function(err, results) {
     if (err) {
         console.log('FINAL ERR: '+err);
-    }
-    else {
-        console.log('BOOKInstances: '+bookinstances);
-        
     }
     // All done, disconnect from database
     mongoose.connection.close();
